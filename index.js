ed625d97e4f21edd5d37c2998aa8bc57d9e66e20
SrHatcher <erickomaldonado11@gmail.com>
2022-10-11 20:32:57 -0600

    project finished
    i thought this project would be easy, but it was a bit hard.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const socialIconsContainer = document.querySelector('.credits__social_media');
+const globeArrow = document.querySelector('.credits__globe')
 const shareButton = document.querySelector('.credits__share_button');
+
 
 shareButton.addEventListener('click', ()=>{
     socialIconsContainer.classList.toggle("social_media--show");
+    globeArrow.classList.toggle('social_media--show')
     shareButton.classList.toggle("share_button--active")
 })
