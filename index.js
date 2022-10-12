699643497760d7efc9b7e868efd7815037bbce51
SrHatcher <erickomaldonado11@gmail.com>
2022-10-11 19:26:32 -0600

    mobile version finished

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const shareButton = document.querySelector('.credits__share_button');
 
 shareButton.addEventListener('click', ()=>{
     socialIconsContainer.classList.toggle("social_media--show");
+    shareButton.classList.toggle("share_button--active")
 })
